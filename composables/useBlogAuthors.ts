923435c4b17725f589be6f33599221540cd3dda4
Bart Ledoux <bledoux@kestra.io>
2025-04-24 11:10:52 +0200

    use typescript for better autocompletion

--- composables/useBlogAuthors.ts
+++ composables/useBlogAuthors.ts
@@ -1,4 +1,10 @@
-export const useBlogAuthors = (blog) => {
+export const useBlogAuthors = (blog: {
+    authors?: { name: string }[];
+    author?: { name: string };
+    authorName?: string;
+    authorImage?: string;
+    authorDescription?: string;
+}) => {
     const getAuthors = () => {
         if (blog.authors && blog.authors.length > 0) {
             return blog.authors;
@@ -18,4 +24,4 @@ export const useBlogAuthors = (blog) => {
         getAuthors,
         getFirstAuthorName
     };
-}; 
+};
